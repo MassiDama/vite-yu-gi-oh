9880c3d0ffdf76bf07a62127bb462a92a3e2f257
Massidama <massidama@outlook.it>
2024-01-08 16:11:14 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+// npm install axios
+import axios from 'axios';
+
+</script>
+
+<template>
+  
+</template>
+
+<style lang="scss" scoped>
+@use './styles/general.scss' as *;
+@use './styles/partials/variables' as *;
+
+</style>
